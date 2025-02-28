0763c91417a78b551611eca19a417c7bb4dc6339
Joepkl <joep.klaassen9@gmail.com>
2025-02-28 14:49:02 +0100

    Created update-cobj endpoint

--- index.js
+++ index.js
@@ -10,11 +10,46 @@ app.use(express.json());
 
 const PRIVATE_APP_ACCESS = process.env.PAT_TOKEN;
 
+const headers = {
+  Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+  "Content-Type": "application/json",
+};
+
+const ufcFightersObjectTypeId = "2-139796957";
+// const ufcFightersUrl = `https://api.hubapi.com/crm/v3/schemas/${ufcFightersObjectTypeId}`;
+
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get("/update-cobj", async (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post("/update-cobj", async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${ufcFightersObjectTypeId}`;
+
+  const body = {
+    properties: {
+      fighter_name: req.body["fighter-name"],
+      fighting_style: req.body["fighting-style"],
+      fight_record: req.body["fight-record"],
+    },
+  };
+
+  try {
+    await axios.post(url, body, { headers });
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.render("updates", {
+      title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+      error: "There was an issue processing your request. Please try again.",
+    });
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
